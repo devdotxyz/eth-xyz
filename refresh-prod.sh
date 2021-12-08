83475234d39a041cc86aa2f4c6556d00bb72ddd3
Brendan DeBeasi <b@dev.xyz>
2021-12-08 09:46:46 -0800

    - Atomic build process

--- refresh-prod.sh
+++ refresh-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git pull;
 node ace build --production --ignore-ts-errors;
+mv ../eth-xyz-live ../eth-xyz-previous;
 mv build ../eth-xyz-live;
 rm -rf ../eth-xyz-previous
-mv ../eth-xyz-live ../eth-xyz-previous;
 cp .env ../eth-xyz-live/.env;
 cd ../eth-xyz-live;
 forever stopall;
