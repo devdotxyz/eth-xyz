e73f3f159e1f606d7973dd54d29ae52a9f996f19
Brendan DeBeasi <b@dev.xyz>
2021-12-13 12:52:13 -0800

    Combine about site and wildcard resolver into one to allow additional logic | Refactor landing index action to be a little clearer | Support for subdomains on main domain

--- start/routes.ts
+++ start/routes.ts
@@ -23,6 +23,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/404', 'LandingController.404')
 Route.get('/setup', 'SetupController.index')
 Route.post('/setup/submit', 'SetupController.submit')
-Route.get('/:domain?', 'LandingController.index')
+Route.get('/:domainAsPath?', 'LandingController.index')
 Route.get('/text-records/:domain', 'LandingController.textRecords')
 Route.get('/nfts/:ethWallet', 'LandingController.nfts')
