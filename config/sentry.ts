9fd3056a9891f6d5807e96bc4627fc2811edf7a9
Brendan DeBeasi <b@dev.xyz>
2022-06-02 13:57:04 -0700

    TS Fixes

--- config/sentry.ts
+++ config/sentry.ts
@@ -1,15 +1,8 @@
 import Env from '@ioc:Adonis/Core/Env'
 
-if (Env.get('SENTRY_DSN') !== '') {
-  const sentryConfig = {
-    dsn: Env.get('SENTRY_DSN'),
-    enabled: true,
-  }
-} else {
-  const sentryConfig = {
-    dsn: Env.get('SENTRY_DSN'),
-    enabled: false,
-  }
+const sentryConfig = {
+  dsn: Env.get('SENTRY_DSN'),
+  enabled: Env.get('SENTRY_DSN') !== '',
 }
 
 export default sentryConfig;
