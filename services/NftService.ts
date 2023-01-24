ae0edcc3cde1bab537400abf040b347ccc154ca3
Paul Makhnovskiy <makhnovskiy@gmail.com>
2023-01-23 16:51:38 -0800

    Returns only what is used for the UI, fetch more from opensea per call

--- services/NftService.ts
+++ services/NftService.ts
@@ -19,10 +19,31 @@ export default class NftService {
 
     do {
       let responseStep = await this.loadData(ethWalletAddress, offset).then(response => {
-        return JSON.parse(<string>response).assets;
+        let assets = JSON.parse(<string>response).assets;
+        return assets.map((asset)=> {
+          return {
+            'id': asset['id'], 
+            'image_url': asset['image_url'], 
+            'name': asset['name'],
+            'description': asset['description'],  
+            'animation_original_url': asset['animation_original_url'],
+            'animation_url': asset['animation_url'],
+            'permalink': asset['permalink'],
+            'asset_contract': {
+              'address': asset['asset_contract']['address']
+            },
+            'creator': {
+              'user': {
+                'username': asset['creator'] && asset['creator']['user']  && asset['creator']['user']['username']
+              },
+              'profile_img_url': asset['creator'] && asset['creator']['profile_img_url']
+            }
+          }
+        })
+
       });
       response = response.concat(responseStep);
-      offset += 50;
+      offset += 200;
 
       // Not really sure why I need to explicitly break, but apparently I do
       if(responseStep.length == 0) {
@@ -47,7 +68,7 @@ export default class NftService {
     let options = {
       hostname: 'api.opensea.io',
       port: 443,
-      path: `/api/v1/assets?owner=${ethWalletAddress}&order_direction=desc&offset=${offset}&limit=50`,
+      path: `/api/v1/assets?owner=${ethWalletAddress}&order_direction=desc&offset=${offset}&limit=200`,
       method: 'GET',
       headers: {
         'X-API-KEY': Env.get('OPENSEA_API_KEY')
