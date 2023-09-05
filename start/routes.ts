87c30249df821dd1e8f59f96a9e9a2d40e20f347
Johnny Cho <jc@dev.xyz>
2023-09-05 16:14:49 -0700

    Remove setup routes #CU-866avq3kp

--- start/routes.ts
+++ start/routes.ts
@@ -22,8 +22,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/404', 'LandingController.404')
 Route.get('/privacy-policy', 'LandingController.privacyPolicy')
-Route.get('/setup', 'SetupController.index')
-Route.post('/setup/submit', 'SetupController.submit')
 Route.post('/clear-profile-cache', 'LandingController.clearProfileCache')
 Route.get('/:domainAsPath?', 'LandingController.index')
 Route.get('/text-records/:domain', 'LandingController.textRecords')
